ec956cfb37177751ff6ec8d17d87add9cbd7fe79
KhadijaMomar <souarekhadija12@gmail.com>
2025-01-30 17:06:32 +0100

    ok2

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    include_once __DIR__ ."/src/header.inc.php";
+?>
+<body>
+    <header>
+        <h1><?= title ?></h1>
+    </header>
+    <picture>
+        <source srcset="./asset/cover.png"
+                media="(orientation: portrait)">
+        
+    </picture>
+    <main>
+        <!-- partie dynamique: date et version -->
+      
+        <h2>Nous sommes le 
+          <time datetime="<?= $_datetime ?>"><?= $_date_php ?></time> 
+        </h2>
+            <p>
+           
+
+                Mise à jour PHP <?= $_modif_php ?> <br>
+            </p>
+        <!-- end -->
+        <!-- structure d'affichage pour les articles -->
+        
+    <?php
+        include_once __DIR__ ."/src/connect.inc.php";
+    ?>
+   <!--end -->
+    <nav>
+        <a class="button" href="search.php">
+            Rechercher les articles
+        </a>
+     
+      
+
+    </nav>
+    </main>
+   <!--  footer -->
+    <footer>
+        <p>&copy; - MIT - <time 
+        datetime="<?= $_datetime ?>">
+        <?= $_date_php ?>
+        </time></p>
+    </footer>
+   
+</body>
+</html>
---
+++ modele/bdd.php
@@ -0,0 +1,12 @@
+<?php
+//Connexion
+$_pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+$_bdd = new PDO('mysql:host=localhost;
+                dbname=posts',
+                'root', 'root',
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$_pdo_options));
+
+$_response = $_bdd->query('SELECT * FROM post ORDER BY date_modification ASC LIMIT 10');
+
+
+?>
